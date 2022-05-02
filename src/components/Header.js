f7592fbbfc736eed491f6ca95cd2c8a04723d0e0
heinminthant <heinminthant.17199@gmail.com>
2022-05-02 16:50:19 +0630

    Implemented CSS modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./Header.css";
+import styles from "./Header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
 const Header = () => {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h3>Where in the world?</h3>
-      <div className="theme-container">
-        <FontAwesomeIcon className="dark-icon" icon={faMoon} rotation={360} />
+      <div className={styles["theme-container"]}>
+        <FontAwesomeIcon
+          className={styles["dark-container"]}
+          icon={faMoon}
+          rotation={360}
+        />
         <p>Dark Mode</p>
       </div>
     </div>
