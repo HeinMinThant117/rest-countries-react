bb2e264f1a2fd57fce06b49be9ae9cab36e349b1
heinminthant <heinminthant.17199@gmail.com>
2022-05-02 16:30:07 +0630

    Font size tweak for header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <SearchBar />
     </div>
   );
 };
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return <div>
+        Hi
+    </div>
+}
+
+export default SearchBar;
