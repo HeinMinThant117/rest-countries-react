864fe46c06864008d861b4ddc8ec7cfea42caab5
heinminthant <heinminthant.17199@gmail.com>
2022-05-05 14:39:00 +0630

    Added modal for country filter

--- src/components/CountryFilter.js
+++ src/components/CountryFilter.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import styles from "./CountryFilter.module.css";
 
 const CountryFilter = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <p>Filter by Region</p>
-      <FontAwesomeIcon icon={faAngleDown} />
+    <div className={styles["filter-container"]}>
+      <div className={styles["filter-button"]}>
+        <p>Filter by Region</p>
+        <FontAwesomeIcon icon={faAngleDown} />
+      </div>
+      <ul className={styles["filter-modal"] + " poggers"}>
+        <li>Africa</li>
+        <li>America</li>
+        <li>Asia</li>
+        <li>Europe</li>
+        <li>Oceania</li>
+      </ul>
     </div>
   );
 };
